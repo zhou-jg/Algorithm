7ab40159d19ea0fad5f709be142bf1dcd9f9cb54
zhou-jg <zjg_robin@hotmail.com>
2016-09-26 10:11:14 +0800

    增加两个应用类问题的解法

---
+++ src/basic/number/applications/LockerDistance.java
@@ -0,0 +1,49 @@
+package basic.number.applications;
+
+import java.util.Objects;
+
+import util.Algorithm;
+
+/**
+ * 在物流行业中，快递公司要在城区（小区）布置一系列的储物柜用来存放快递包裹。为方便用户，需要这些储物柜的位置
+ * 尽量靠近小区。
+ * @author Jingang Zhou
+ *
+ */
+public class LockerDistance {
+	/**
+	 * 例如，假设某个城区以街区为单位，长有 3 个街区，宽有 5 个街区，储物柜的位置在第一个街区块内，那么每个街区块
+	 * 到离自己最近的距离分别为（距离以水平距离和垂直距离的和为准）：
+	 * 0	1	2
+	 * 1	2	3
+	 * 2	3	4
+	 * 3	4	5
+	 * 4	4	6
+	 * @param cityLength 城市长度，以街区（block）为单位， 大于 0
+	 * @param cityWidth 城市宽度，以街区为单位，大于0
+	 * @param xPos	储物柜所在街区的x坐标 （起始为1，方向向右）
+	 * @param yPos	储物柜所在街区的y坐标（起始为1，方向向下）
+	 * @return 每个街区距离最近的储物柜位置
+	 */
+	@Algorithm(kind = "application")
+	public static int[][] distance(int cityLength, int cityWidth, int[] xPos, int[] yPos){
+		Objects.requireNonNull(xPos);
+		Objects.requireNonNull(yPos);
+		//需要xPos和yPos的长度相等，略去各种合法性验证
+		
+		int[][] result = new int[cityLength][cityWidth];
+		for (int i=0; i<cityLength; i++){
+			for (int j=0; j<cityWidth; j++){
+				int minDistance = cityLength + cityWidth - 2;				
+				for (int k=0; k<xPos.length; k++){
+					int distance = Math.abs(xPos[k] - i - 1) + Math.abs(yPos[k] - j - 1);
+					if (distance < minDistance){
+						minDistance = distance;
+					}
+				}
+				result[i][j] = minDistance;
+			}
+		}
+		return result;
+	}
+}
---
+++ src/basic/sorting/applications/CourseRecommendation.java
@@ -0,0 +1,101 @@
+package basic.sorting.applications;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import util.Algorithm;
+
+public class CourseRecommendation {
+	
+	/**
+	 * 依据某人的朋友所参加的课程来向某个推荐他还没用参加的课程。
+	 * 这里的朋友指的是直接（1度）和2度朋友（朋友的朋友）。
+	 * 简单起见，朋友和课程都以字符串表示。所推荐的课程以朋友参与的多少降序排列。
+	 * @param user 给定需要推荐课程的用户
+	 * @return 受欢迎度降序排列的课程列表
+	 */
+	@Algorithm(kind = "application")
+	public static List<String> getCourseToRecommend(String user){
+		//获取所有的朋友
+		List<String> directFriends = getDirectFriends(user);
+		Set<String> allFriends = new HashSet<>(directFriends);
+		for (String friend : directFriends){
+			allFriends.addAll(getDirectFriends(friend));
+		}
+		allFriends.remove(user);
+		
+		//构建所有需要考虑的课程及其欢迎度
+		Map<String, Integer> course2Number = new HashMap<>();
+		Set<String> alreadyAttendedCourses = new HashSet<>(getAttendedCourses(user));
+		for (String friend : allFriends){
+			List<String> courses = getAttendedCourses(friend);
+			for (String course : courses){
+				if (alreadyAttendedCourses.contains(course)){
+					continue;
+				}else if (course2Number.containsKey(course)){
+					course2Number.put(course, course2Number.get(course)+1);
+				}else{
+					course2Number.put(course, 1);
+				}
+			}
+		}
+		
+		//排序
+		List<CourseNumber> courses = new ArrayList<>(course2Number.size());
+		for (String course : course2Number.keySet()){
+			courses.add(new CourseNumber(course, course2Number.get(course)));
+		}
+		Collections.sort(courses, new Comparator<CourseNumber>() {
+			public int compare(CourseNumber c1, CourseNumber c2) {
+				return c2.getNumber() - c1.getNumber();
+			};
+		});
+		
+		//构造结果
+		List<String> result = new ArrayList<>(courses.size());
+		for (CourseNumber c : courses){
+			result.add(c.getCourse());
+		}
+		
+		return result;
+	}
+	
+	//helper class for sorting
+	static class CourseNumber{
+		private final String course;
+		private final int number;
+		CourseNumber(String course, int number){
+			this.course = course;
+			this.number = number;
+		}
+		
+		public String getCourse(){
+			return course;
+		}
+		
+		public int getNumber(){
+			return number;
+		}
+	}
+	
+	/*
+	 * 获得某个直接的（1度）朋友
+	 * mock方法
+	 */
+	static List<String> getDirectFriends(String user){
+		return Collections.emptyList();
+	}
+	/*
+	 * 获得某个注册的课程
+	 * mock方法
+	 */	
+	static List<String> getAttendedCourses(String user){
+		return Collections.emptyList();
+	}
+}
