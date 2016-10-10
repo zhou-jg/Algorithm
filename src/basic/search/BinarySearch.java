285b3152a55b425ecdcdcd55cbf03505b12e9c2a
zhou-jg <zjg_robin@hotmail.com>
2016-10-10 13:27:32 +0800

    补充了基于Comparator来比较的二分查找

--- src/basic/search/BinarySearch.java
+++ src/basic/search/BinarySearch.java
@@ -1,5 +1,6 @@
 package basic.search;
 
+import java.util.Comparator;
 import java.util.List;
 
 import util.Algorithm;
@@ -42,6 +43,40 @@ public class BinarySearch {
 			return internalFind(list, t, begin, index-1);
 		}else{
 			return internalFind(list, t, index+1, end);
+		}
+	}
+	
+	/**
+	 * 根据给定的 {@code java.util.Comparator} 来定序。
+	 * 实现为升序。
+	 * @param list 有序列表
+	 * @param t 给定值
+	 * @return 给定值在列表的有序位置
+	 */
+	@Algorithm(kind="search")
+	public static <T> int find(List<T> list, T t, Comparator<T> c){
+		if (list == null){
+			return -1;
+		}
+		if (list.size() == 0){
+			return 0;
+		}
+		
+		return internalFind(list, t, c, 0, list.size()-1);		
+	}
+	
+	private static <T> int internalFind(List<T> list, T t, Comparator<T> c, int begin, int end){
+		if (begin > end){
+			return begin;
+		}
+		int index = (begin+end)/2;
+		T other = list.get(index);
+		if (c.compare(t, other) == 0){
+			return index;
+		}else if (c.compare(t, other) < 0){
+			return internalFind(list, t, c, begin, index-1);
+		}else{
+			return internalFind(list, t, c, index+1, end);
 		}
 	}
 }
--- test/basic/search/BinarySearchTest.java
+++ test/basic/search/BinarySearchTest.java
@@ -19,15 +19,18 @@ public class BinarySearchTest {
 	@Test
 	public void atBeginning() {
 		assertEquals(0, BinarySearch.find(list, 1));
+		assertEquals(0, BinarySearch.find(list, 1, (c1, c2)-> c1-c2));
 	}
 
 	@Test
 	public void atMiddle() {
 		assertEquals(1, BinarySearch.find(list, 3));
+		assertEquals(1, BinarySearch.find(list, 3, (c1, c2)-> c1-c2));
 	}
 	
 	@Test
 	public void atEnd() {
 		assertEquals(4, BinarySearch.find(list, 6));
+		assertEquals(4, BinarySearch.find(list, 6, (c1, c2)-> c1-c2));
 	}
 }
