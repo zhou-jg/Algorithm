b8bcfd2502b9724254dfa1d82fd0487a082d875a
zhou-jg <zjg_robin@hotmail.com>
2016-10-22 17:36:23 +0800

    补充根据输入数组层次化构造完全二叉树的算法

--- src/datastructure/BiTree.java
+++ src/datastructure/BiTree.java
@@ -1,5 +1,10 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import util.Algorithm;
+
 public class BiTree<T>{
 	private BiTree<T> left, right;
 	private T data;
@@ -43,5 +48,28 @@ public class BiTree<T>{
 	@Override
 	public String toString(){
 		return data.toString();
+	}
+	
+	/**
+	 * 按照层次依序构造完全二叉树
+	 * @param data 给定的数据
+	 * @return 二叉树的根节点
+	 */
+	@Algorithm("树")
+	public static <T> BiTree<T> buildTree(T[] data){
+		if (data == null || data.length == 0){
+			return null;
+		}
+		List<BiTree<T>> tree = new ArrayList<BiTree<T>>(data.length);
+		for (int i=0; i<data.length; i++){
+			tree.add(new BiTree<T>(data[i]));
+		}
+		for (int i=0; i<data.length/2; i++){
+			tree.get(i).setLeft(tree.get(2*i+1));
+			if (2*i+2 <= data.length-1){
+				tree.get(i).setRight(tree.get(2*i+2));
+			}
+		}
+		return tree.get(0);
 	}
 }
