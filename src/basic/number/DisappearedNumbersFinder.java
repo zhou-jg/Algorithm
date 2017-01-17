47ca6ea606aed174a01ff8a0854272235bfa2d85
zhou-jg <zhou-jg@users.noreply.github.com>
2017-01-17 13:31:30 +0800

    在长度为n、元素值为1..n的数组中找出未出现的值

---
+++ src/basic/number/DisappearedNumbersFinder.java
@@ -0,0 +1,41 @@
+package basic number;
+
+/**
+source: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), 
+some elements appear twice and others appear once. Find all the 
+elements of [1, n] inclusive that do not appear in this array.
+Could you do it without extra space and in O(n) runtime? 
+You may assume the returned list does not count as extra space.
+
+Example:
+
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[5,6]
+*/
+public class DisappearedNumbersFinder {
+  public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ret = new ArrayList<Integer>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]) - 1;
+            if(nums[val] > 0) {
+                nums[val] = -nums[val];
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > 0) {
+                ret.add(i+1);
+            }
+        }
+        for (int i=0; i<nums.length; i++){
+          nums[i] = Math.abs(nums[i]);
+        }
+        return ret;
+    }
+ }
