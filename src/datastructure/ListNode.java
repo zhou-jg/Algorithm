99c60512440fa0670e5e0c7bc62eaf856646fbab
zhou-jg <zjg_robin@hotmail.com>
2017-02-03 13:24:37 +0800

    ListNode数据结构

---
+++ src/datastructure/ListNode.java
@@ -0,0 +1,10 @@
+package datastructure;
+
+/**
+ * Created by zhou-jg on 2017/2/3.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
