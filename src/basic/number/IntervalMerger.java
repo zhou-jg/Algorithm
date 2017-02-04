b99283e73757547d5fb045ff6b1afb4424528085
zhou-jg <zjg_robin@hotmail.com>
2017-02-04 09:05:39 +0800

    bugfix：import Collections类

--- src/basic/number/IntervalMerger.java
+++ src/basic/number/IntervalMerger.java
@@ -1,9 +1,6 @@
 package basic.number;
 
-import java.util.List;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 /**
 Source: https://leetcode.com/problems/merge-intervals/
