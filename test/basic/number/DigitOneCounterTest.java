4716bcb407737cea6e8785088b8f3a7b0d568eff
zhou-jg <zjg_robin@hotmail.com>
2017-02-08 09:23:28 +0800

    小于n的数中数字1出现的次数

--- test/basic/number/DigitOneCounterTest.java
+++ test/basic/number/DigitOneCounterTest.java
@@ -8,12 +8,21 @@ import static org.junit.Assert.*;
  * Created by zhou-jg on 2017/1/16.
  */
 public class DigitOneCounterTest {
+
+    DigitOneCounter doc = new DigitOneCounter();
     @Test
     public void count() throws Exception {
-        DigitOneCounter doc = new DigitOneCounter();
         assertEquals(1, doc.count(9));
         assertEquals(5, doc.count(12));
         assertEquals(689, doc.count(1234));
+    }
+
+    @Test
+    public void count2(){
+        assertEquals(1, doc.count(9));
+        assertEquals(5, doc.count(12));
+        assertEquals(689, doc.count(1234));
+        assertEquals(40, doc.count2(200));
     }
 
 }
