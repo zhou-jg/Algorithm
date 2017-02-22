b0d290e66e0c0f537d7c0683c5ddd5b36e10df1b
zhou-jg <zjg_robin@hotmail.com>
2017-02-22 12:07:37 +0800

    补充简易树节点用于其它树算法

---
+++ src/datastructure/TreeNode.java
@@ -0,0 +1,13 @@
+package datastructure;
+
+/**
+ * Just a convenient class for algorithms.
+ *
+ * <p>Created by zhou-jg on 2017/2/22.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
